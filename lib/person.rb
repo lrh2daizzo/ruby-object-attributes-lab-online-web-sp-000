c03e1d96c6b3670678ebda7b141eda2a41e8f8ea
lrh2daizzo <herrera.lrh@gmail.com>
2023-03-12 16:37:46 -0500

    reset

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,3 @@
 class Person 
-    def name=(name)
-        @name = name 
-    end
-
-    def name 
-        @name 
-    end
-
-    def job=(job)
-        @job = job 
-    end
-
-    def job 
-        @job 
-    end
+   
 end
